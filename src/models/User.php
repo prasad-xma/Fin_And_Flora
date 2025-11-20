cb10d78976c1f212758ae2ae7d41d7d135f1a308
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-11-21 02:20:40 +0530

    create user model

--- src/models/User.php
+++ src/models/User.php
@@ -8,8 +8,21 @@ class User {
         $this->collection = $db->selectCollection('users');
     }
 
-    public function createUser($data) {
-        
+    public function createUser($data) 
+    {
+        return $this->collection->insertOne([
+            'first_name' => $data['first_name'],
+            'last_name'  => $data['last_name'],
+            'email'      => $data['email'],
+            'password'   => password_hash($data['password'], PASSWORD_DEFAULT),
+            'role'       => $data['role'],
+            'created_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+
+    public function findByEmail($email) 
+    {
+        return $this->collection->findOne(['email' => $email]);
     }
 }
 ?>
